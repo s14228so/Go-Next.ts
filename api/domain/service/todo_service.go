a9c7a352e764f6985e729f92f3dbf4503d24b206
s14228so <saongtx7@gmail.com>
2019-12-26 21:42:55 +0900

    create service

---
+++ api/domain/service/todo_service.go
@@ -0,0 +1,85 @@
+package service
+
+// user周りのメソッド。DBとの通信周り
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/s14228so/todo/api/infrastructure/db"
+)
+
+// Service procides user's behavior
+type Service struct{}
+
+// GetUserAll is get all User
+func (s Service) GetTodoAll() ([]User, error) {
+	db := db.GetDB()
+	var u []Todo
+
+	if err := db.Find(&u).Error; err != nil {
+		return nil, err
+	}
+
+	for i, todo := range u {
+
+		if err := db.Model(&todo).Related(&todo.User, "User"); err != nil {
+			fmt.Println("プランが見つかりませんでした")
+		}
+		u[i] = user
+	}
+
+	return u, nil
+}
+
+// CreateUserModel is create User model
+func (s Service) CreateUserModel(c *gin.Context) (User, []error) {
+	db := db.GetDB()
+	var u Todo
+
+	if err := c.BindJSON(&u); err != nil {
+		var errors []error
+		errors = append(errors, err)
+		return u, errors
+	}
+
+	return u, nil
+}
+
+// GetUserByID is get a User
+func (s Service) GetTodoByID(id string) (User, error) {
+	db := db.GetDB()
+	var u Todo
+	// var coach entity.Coach
+	return u, nil
+}
+
+// UpdateUserByID is update a User
+func (s Service) UpdateTodorByID(id string, c *gin.Context) (User, error) {
+	db := db.GetDB()
+	var u User
+
+	if err := db.Where("id = ?", id).First(&u).Error; err != nil {
+		fmt.Println(err)
+		log.Printf("err: %v", err)
+		return u, err
+	}
+
+	db.Save(&u)
+
+	return u, nil
+}
+
+// DeleteUserByID is delete a User
+func (s Service) DeleteTodoByID(id string) error {
+	db := db.GetDB()
+	var u User
+
+	if err := db.Where("id = ?", id).Delete(&u).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ api/domain/service/user_service.go
@@ -0,0 +1,112 @@
+package service
+
+// user周りのメソッド。DBとの通信周り
+
+import (
+	"errors"
+	"fmt"
+	"log"
+
+	"github.com/jinzhu/gorm"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/s14228so/todo/api/infrastructure/db"
+)
+
+// Service procides user's behavior
+type Service struct{}
+
+// Validate is User validate
+func (user User) Validate(db *gorm.DB) {
+	if user.Email == "" {
+		db.AddError(errors.New("Please be sure to enter your email address"))
+	}
+}
+
+//GetUserByEmail is
+func (s Service) GetUserByEmail(email string) (User, error) {
+	db := db.GetDB()
+	var u User
+	// var coach entity.Coach
+
+	if err := db.Where("Email = ?", email).First(&u).Error; err != nil {
+		return u, err
+	}
+
+	if err := db.Model(&u).Related(&u.Todos, "Todos"); err != nil {
+		fmt.Println("プランが見つかりませんでした")
+	}
+
+	return u, nil
+}
+
+// GetUserAll is get all User
+func (s Service) GetUserAll() ([]User, error) {
+	db := db.GetDB()
+	var u []User
+
+	if err := db.Find(&u).Error; err != nil {
+		return nil, err
+	}
+
+	for i, user := range u {
+
+		if err := db.Model(&user).Related(&user.Todos, "Todos"); err != nil {
+			fmt.Println("プランが見つかりませんでした")
+		}
+		u[i] = user
+	}
+
+	return u, nil
+}
+
+// CreateUserModel is create User model
+func (s Service) CreateUserModel(c *gin.Context) (User, []error) {
+	db := db.GetDB()
+	var u User
+
+	if err := c.BindJSON(&u); err != nil {
+		var errors []error
+		errors = append(errors, err)
+		return u, errors
+	}
+
+	return u, nil
+}
+
+// GetUserByID is get a User
+func (s Service) GetUserByID(id string) (User, error) {
+	db := db.GetDB()
+	var u User
+	// var coach entity.Coach
+	return u, nil
+}
+
+// UpdateUserByID is update a User
+func (s Service) UpdateUserByID(id string, c *gin.Context) (User, error) {
+	db := db.GetDB()
+	var u User
+
+	if err := db.Where("id = ?", id).First(&u).Error; err != nil {
+		fmt.Println(err)
+		log.Printf("err: %v", err)
+		return u, err
+	}
+
+	db.Save(&u)
+
+	return u, nil
+}
+
+// DeleteUserByID is delete a User
+func (s Service) DeleteUserByID(id string) error {
+	db := db.GetDB()
+	var u User
+
+	if err := db.Where("id = ?", id).Delete(&u).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
