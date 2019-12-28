3b4e4aa8abd980852a9dd9ea0e318b0f565317df
s14228so <saongtx7@gmail.com>
2019-12-28 21:27:38 +0900

    delete action router.push

--- web/components/ListDetail.tsx
+++ web/components/ListDetail.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 
 import { Todo } from '../interfaces'
+import axios from "axios"
+import { useRouter } from 'next/router'
 
 type ListDetailProps = {
   item: Todo
@@ -8,12 +10,32 @@ type ListDetailProps = {
 
 const ListDetail: React.FunctionComponent<ListDetailProps> = ({
   item: todo,
-}) => (
+}) => {
+
+  const router = useRouter()
+
+  const handleDelete = async () => {
+
+    const result = confirm("本当に削除しますか？")
+    if (result) {
+
+      const { data } = await axios.delete(`http://localhost:8080/todos/${todo.id}`)
+      console.log(data, "is deleted!")
+
+      router.push("/todos")
+    }
+
+
+  }
+
+  return (
     <div>
       <h1>{todo.title}</h1>
       <p>ID: {todo.id}</p>
       <p>ユーザー名: {todo.user.username}</p>
+      <button onClick={handleDelete}>削除</button>
     </div>
   )
+}
 
 export default ListDetail
